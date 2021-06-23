6572bf0825f4cce03e9eb139b7b61041a0faac0b
Ktosox <ktoso3@gmail.com>
2021-06-23 19:01:44 +0200

    re-arranged the dialog window to include a place for a portrait

--- Game/Managers/Dialog.gd
+++ Game/Managers/Dialog.gd
@@ -9,12 +9,12 @@ func _ready():
 	EM.dialogScene = self
 
 func display(text:String):
-	$TextBox.visible = true
-	$TextBox.bbcode_text = "[center]"+text+"[/center]"
+	$Window/Layout/TextBox.visible = true
+	$Window/Layout/TextBox.bbcode_text = "[center]"+text+"[/center]"
 	$VisibleTimer.start(baseTime + letterTime*text.length())
 	pass
 
 
 func _on_VisibleTimer_timeout():
-	$TextBox.visible = false
+	$Window/Layout/TextBox.visible = false
 	pass # Replace with function body.
